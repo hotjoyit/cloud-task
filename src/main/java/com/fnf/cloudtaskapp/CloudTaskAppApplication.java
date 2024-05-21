74a7bc2071cb91b4a5f3c337db596807b78fa0df
hotjoyit <hotjoyit@gmail.com>
2024-05-21 13:46:49 +0900

    Cloud Task App 프로젝트 셋업

---
+++ src/main/java/com/fnf/cloudtaskapp/CloudTaskAppApplication.java
@@ -0,0 +1,13 @@
+package com.fnf.cloudtaskapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudTaskAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudTaskAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fnf/cloudtaskapp/CloudTaskAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fnf.cloudtaskapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudTaskAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
